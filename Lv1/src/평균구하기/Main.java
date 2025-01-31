7b2bcb71b49534d3980ebe86203259351219a09b
1zeen2 <dnjswls6878@naver.com>
2025-01-31 23:01:13 +0900

    feat: 2025-01-30 (배열 index값의) 평균 구하기 알고리즘 풀이 완료

--- Lv1/src/평균구하기/Main.java
+++ Lv1/src/평균구하기/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Solution sol = new Solution();
 		
-		int[] arr = {365, 2345, 1242, 986, 2143, 9656, 9835};
+		int[] arr = {365, 2345, 1242, 986, 2143, 9656};
 		
 		System.out.println(sol.solution(arr));
 	}
