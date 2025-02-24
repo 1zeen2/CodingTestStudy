df12098101fe00bdd7b21dbb29faab6cbfd0bef2
1zeen2 <dnjswls6878@naver.com>
2025-02-24 20:43:50 +0900

    feat: 2025-02-24 내적 문제 풀이 완료

---
+++ Lv1/src/내적/Main.java
@@ -0,0 +1,12 @@
+package 내적;
+
+public class Main {
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		
+		int[] a = {5, 29, -30, 0, -7};
+		int[] b = {45, -234, 95, 238, 270};
+		
+		System.out.println(sol.solution(a, b));
+	}
+}
---
+++ Lv1/src/내적/Solution.java
@@ -0,0 +1,19 @@
+package 내적;
+
+import java.util.stream.IntStream;
+
+public class Solution {
+	public int solution(int[] a, int[] b) {
+		if (a.length != b.length || a.length < 1 || 1000 < a.length) {
+			throw new IllegalArgumentException("a의 입력 값의 개수와 b의 입력 값의 개수는 같아야 하며 "
+											 + "각 입력 값의 개수는 1개 이상 1000개 이하만 가능합니다.");	
+		}
+		
+		if (IntStream.of(a).anyMatch(n -> n < -1000 || 1000 < n) || 
+			IntStream.of(b).anyMatch(n -> n < -1000 || 1000 < n)) {
+			throw new IllegalArgumentException("각 입력 값의 크기는 -1000 이상, 1000 이하의 정수만 가능합니다.");
+		}
+		
+		return IntStream.range(0, a.length).map(i -> a[i] * b[i]).sum();
+	}
+}
